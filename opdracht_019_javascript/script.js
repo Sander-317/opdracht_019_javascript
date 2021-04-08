0bc59c210115b3a3f897b723d17675a3a4553712
Sander-317 <80258328+Sander-317@users.noreply.github.com>
2021-04-08 12:09:40 +0200

    Add files via upload

---
+++ opdracht_019_javascript/script.js
@@ -0,0 +1,10 @@
+let name = 'sander';
+let age = 36;
+
+
+console.log("Hello Winc Academy")
+console.log(name)
+console.log(4 + 4)
+console.log('4' + '4')
+console.log(5 + 9 - 4 * 5 / 6)
+console.log(typeof age)
